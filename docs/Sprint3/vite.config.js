4d2b93ef4e869a4ea79ad57da771d387b3823e3e
Ichi6o05 <diegoacevedomazuela@gmail.com>
2025-05-16 01:37:48 -0400

    intentando subir el proyecto

--- docs/Sprint3/vite.config.js
+++ docs/Sprint3/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-  base: '/Front-end/docs/Sprint3/', 
+  base: 'https://Ichi6o05.github.io/Front-end/Sprin3/', 
   plugins: [react()],
 })
