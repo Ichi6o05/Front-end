cef36f668544fa3193316697c19e3652dca717e8
Ichi6o05 <107375954+Ichi6o05@users.noreply.github.com>
2025-05-16 01:14:43 -0400

    Update vite.config.js
    cambios en vite config

--- docs/Sprint3/vite.config.js
+++ docs/Sprint3/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-  base: '/Front-end/docs/Sprint3/', 
   plugins: [react()],
+  base: 'https://Ichi6o05.github.io/Front-end/docs/Sprint3/', 
 })
